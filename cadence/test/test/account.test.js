2079fd8ba7267276792fa17af5fd712732781a8c
script-money <hello@script.money>
2021-05-29 23:33:22 +0800

    :white_check_mark: add first test file

--- cadence/test/test/account.test.js
+++ cadence/test/test/account.test.js
@@ -1,25 +1,41 @@
 import path from "path";
-import { init, getAccountAddress } from "flow-js-testing";
+import { init, getAccountAddress, deployContractByName, getFlowBalance, mintFlow, getContractAddress, deployContract, getTemplate } from "flow-js-testing";
 
-beforeEach(async (done) => {
-  const basePath = path.resolve(__dirname, "../cadence");
-  const port = 8080;
-  init(basePath, port);
-  done();
-});
+const basePath = path.resolve(__dirname, "../../../cadence");
+const port = 8080;
+init(basePath, port);
 
-describe("Accounts", () => {
-  test("Create Accounts", async () => {
-    const Alice = await getAccountAddress("Alice");
-    const Bob = await getAccountAddress("Bob");
-    const Charlie = await getAccountAddress("Charlie");
-    const Dave = await getAccountAddress("Dave");
+describe("NonFungibleToken deploy", () => {
 
-    console.log("Four accounts were created with following addresses:\n", {
-      Alice,
-      Bob,
-      Charlie,
-      Dave,
-    });
+  // 挖一些测试币
+  test("get account FLOW balance", async () => {
+    const newAccountAddress = await getAccountAddress("Admin");
+    const balance = await getFlowBalance(newAccountAddress);
+    console.log({ balance });
+    await mintFlow(newAccountAddress, "10.572");
+    const newBalance = await getFlowBalance(newAccountAddress);
+    console.log({ newBalance });
   });
+
+  // 部署两个合约
+  it('should be deploy two contract', async () => {
+    const adminAddress = await getAccountAddress("Admin");
+
+    try {
+      await deployContractByName({ to: adminAddress, name: "NonFungibleToken" });
+    } catch (e) {
+      console.warn('部署NonFungibleToken出错', e);
+    }
+    const NonFungibleTokenContract = await getContractAddress("NonFungibleToken");
+    expect(NonFungibleTokenContract).toEqual(adminAddress)
+
+    let deployTx;
+    try {
+      deployTx = await deployContractByName({ to: adminAddress, name: "FanNFT", addressMap: { NonFungibleToken: adminAddress } })
+    } catch (e) {
+      console.warn(`部署FanNFT出错`, e);
+    }
+    expect(deployTx.errorMessage).toBe("");
+    expect(deployTx.status).toBe(4); // 4是正常
+  })
 });

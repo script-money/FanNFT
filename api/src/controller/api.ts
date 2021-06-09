2398f797bb91c1b9772ac4d4df78396c7c248360
脚本刷钱 <hello@script.money>
2021-06-09 09:37:52 +0800

    :fire: remove ts version api

--- api/src/controller/api.ts
+++
@@ -1,9 +0,0 @@
-import { Inject, Controller, Provide } from '@midwayjs/decorator';
-import { Context } from 'egg';
-
-@Provide()
-@Controller('/api')
-export class APIController {
-  @Inject()
-  ctx: Context;
-}
